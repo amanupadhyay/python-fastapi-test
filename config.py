6c4f3fa8c42cef9624320d4a16ddc8828d864f52
upadhyayaman <aman.upadhyay@annalect.com>
2023-06-26 18:54:09 +0530

    changes in settings class

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    DB_USER =  os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST= os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT')
-    DB_NAME = os.getenv('DB_NAME')
-    DB_SSL_MODE = os.getenv('DB_SSL_MODE')
+    DB_USER:str =  os.getenv('DB_USER')
+    DB_PASSWORD:str = os.getenv('DB_PASSWORD')
+    DB_HOST:str = os.getenv('DB_HOST')
+    DB_PORT:str = os.getenv('DB_PORT')
+    DB_NAME:str = os.getenv('DB_NAME')
+    DB_SSL_MODE:str = os.getenv('DB_SSL_MODE')
 
     DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode={DB_SSL_MODE}"
 
